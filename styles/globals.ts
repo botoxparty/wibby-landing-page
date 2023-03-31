6ef0bebeb47b90e004ff811da3f17defc22c7152
Adam Hammad <mail@adamham.dev>
2023-03-31 19:06:00 +0200

    remove globals

--- styles/globals.ts
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    font-family: var(--font-mono);
-  }`;
-
-export default GlobalStyle;
